ed6a17e51d6fda15ef366a996b05ebe1452a1b75
christophersii <116040417+christophersii@users.noreply.github.com>
2023-02-28 16:07:39 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+$db_host= "localhost";
+$db_user= "b005c8a97ae61d";
+$db_pass= "04eca8ce";
+&db_name= "heroku_3442ee38bf9fb24";
+
+$connect= mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die ("Error connecting to database");
+
+?>
