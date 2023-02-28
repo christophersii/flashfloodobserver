dd4e565ce4472beee519b814e99de4a5cbc16ff1
christophersii <116040417+christophersii@users.noreply.github.com>
2023-02-28 17:51:16 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,11 +5,11 @@ $query= $connect->query("SELECT * FROM admin");
 
 <table border="1">
     <tr>
-        <td>admin_id</td>
-        <td>admin_username</td>
-        <td>admin_password</td>
-        <td>admin_email</td>
-        <td>admin_phone</td>
+        <td>Admin ID</td>
+        <td>Username</td>
+        <td>Password</td>
+        <td>E-mail</td>
+        <td>Phone</td>
     </tr>
 
     <?php
@@ -18,10 +18,10 @@ $query= $connect->query("SELECT * FROM admin");
         echo "<tr>
             <td>$no</td>
             <td>{$row['admin_id']}</td>
-            <td>{$row['admin_username']}</td>
+            <td>{$row['admin_user']}</td>
             <td>{$row['admin_password']}</td>
-            <td>{$row['admin_email']}</td>
-            <td>{$row['admin_phone']}</td>
+            <td>{$row['admin_address']}</td>
+            <td>{$row['contact_no']}</td>
         </tr>";
         $no++;
     }
