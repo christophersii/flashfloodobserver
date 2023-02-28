0fe8ad6964073435af11eab36eda6cf819fd540b
christophersii <116040417+christophersii@users.noreply.github.com>
2023-02-28 17:44:06 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,29 @@
 <?php
 include("config.php");
 
-$query= $connect->query("SELECT * FROM `admin`");
+$query= $connect->query("SELECT * FROM admin");
 ?>
 
 <table border="1">
     <tr>
-        <th>admin_id</th>
-        <th>admin_username</th>
-        <th>admin_password</th>
-        <th>admin_email</th>
-        <th>admin_phone</th>
+        <td>admin_id</td>
+        <td>admin_username</td>
+        <td>admin_password</td>
+        <td>admin_email</td>
+        <td>admin_phone</td>
     </tr>
 
     <?php
     $no= 1;
     while($row= $query->fetch_assoc()){
-        echo "<tr>";
+        echo "<tr>"
         echo "<td>".$no."</td>";
         echo "<td>".$row['admin_id']."</td>";
         echo "<td>".$row['admin_username']."</td>";
         echo "<td>".$row['admin_password']."</td>";
         echo "<td>".$row['admin_email']."</td>";
         echo "<td>".$row['admin_phone']."</td>";
-        echo "</tr>";å
+        echo "</tr>";
         $no++;
     }
     ?>
