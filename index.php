f742cd1c874c6947e0fa1e58e5a4b40a44ff57bd
christophersii <116040417+christophersii@users.noreply.github.com>
2023-02-28 17:54:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $query= $connect->query("SELECT * FROM admin");
 <table border="1">
     <tr>
         <td>Admin ID</td>
-        <td>Username</td>
+        <td>Admin Name</td>
         <td>Password</td>
         <td>E-mail</td>
         <td>Phone</td>
@@ -17,10 +17,10 @@ $query= $connect->query("SELECT * FROM admin");
     while($row= $query->fetch_assoc()){
         echo "<tr>
             <td>{$row['admin_id']}</td>
-            <td>{$row['admin_user']}</td>
+            <td>{$row['admin_name']}</td>
             <td>{$row['admin_password']}</td>
             <td>{$row['admin_address']}</td>
-            <td>{$row['contact_no']}</td>
+            <td>{$row['admin_contact_no']}</td>
         </tr>";
         $no++;
     }
