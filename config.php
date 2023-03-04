9e9fc85604c6689d6942836bb615363d829ed1d7
christophersii <69385@siswa.unimas.my>
2023-03-04 15:21:53 +0800

    15:21 4 march

--- config.php
+++ config.php
@@ -1,14 +1,18 @@
 <?php
-// $servername= "us-cdbr-east-06.cleardb.net";
-// $username= "b005c8a97ae61d";
-// $password= "04eca8ce";
-// $dbname= "heroku_3442ee38bf9fb24";
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-// $conn= new mysqli($servername, $username, $password, $dbname);
-
-$servername= "localhost";
-$username= "root";
-$password= "";
+$servername= "us-cdbr-east-06.cleardb.net";
+$username= "b005c8a97ae61d";
+$password= "04eca8ce";
 $dbname= "heroku_3442ee38bf9fb24";
 
 $conn= new mysqli($servername, $username, $password, $dbname);
+
+// $servername= "localhost";
+// $username= "root";
+// $password= "";
+// $dbname= "heroku_3442ee38bf9fb24";
+
+// $conn= new mysqli($servername, $username, $password, $dbname);
