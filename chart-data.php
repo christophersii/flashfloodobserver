9331e11d356ca3ca988617abe963b219a063f366
christophersii <69385@siswa.unimas.my>
2023-04-03 17:21:45 +0800

    Update chart-data.php

--- chart-data.php
+++ chart-data.php
@@ -23,14 +23,14 @@ while ($data = $result->fetch_assoc()){
 $readings_time = array_column($sensor_data, 'reading_time');
 
 // ******* Uncomment to convert readings time array to your timezone ********
-/*$i = 0;
+$i = 0;
 foreach ($readings_time as $reading){
     // Uncomment to set timezone to - 1 hour (you can change 1 to any number)
-    $readings_time[$i] = date("Y-m-d H:i:s", strtotime("$reading - 1 hours"));
+    //$readings_time[$i] = date("Y-m-d H:i:s", strtotime("$reading - 1 hours"));
     // Uncomment to set timezone to + 4 hours (you can change 4 to any number)
-    //$readings_time[$i] = date("Y-m-d H:i:s", strtotime("$reading + 4 hours"));
+    $readings_time[$i] = date("Y-m-d H:i:s", strtotime("$reading + 8 hours"));
     $i += 1;
-}*/
+}
 
 $value1 = json_encode(array_reverse(array_column($sensor_data, 'value1')), JSON_NUMERIC_CHECK);
 $value2 = json_encode(array_reverse(array_column($sensor_data, 'value2')), JSON_NUMERIC_CHECK);
