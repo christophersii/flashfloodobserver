7299e3a6aa3f11a410f73aeda1ad1fc66ab52aae
christophersii <116040417+christophersii@users.noreply.github.com>
2023-04-14 05:52:58 +0800

    Update login.php

--- login.php
+++ login.php
@@ -5,13 +5,14 @@
   $admin_email = $_POST['admin_email'];
   $admin_password = $_POST['admin_password'];
 
-  $sql = "SELECT * FROM admin WHERE admin_email = '$admin_email' AND admin_password = '$admin_password'";//get the admin email and password from the database
-  $result = mysqli_query($conn, $sql);//get the result from the database
+  $sql = "SELECT * FROM admin WHERE admin_email = '$admin_email' AND admin_password = '$admin_password'";
+  $result = mysqli_query($conn, $sql);
 
   if (mysqli_num_rows($result) == 1) {
-      $row = mysqli_fetch_assoc($result);//get the row from the database
-      $admin_name = $row["admin_name"];//get the email from the database
-      echo json_encode(["status" => "Success", "admin_name" => $admin_name]);//send the name to the app
+      $row = mysqli_fetch_assoc($result);
+      $admin_id = $row["admin_id"]; // retrieve the admin_id
+      $admin_name = $row["admin_name"];
+      echo json_encode(["status" => "Success", "admin_name" => $admin_name, "admin_id" => $admin_id]); // return the admin_id along with the admin_name
   } else {
       echo json_encode(["status" => "Error"]);
   }
