5295bf3faaeb0ba89938d363f33014243530e163
christophersii <116040417+christophersii@users.noreply.github.com>
2023-04-14 05:02:19 +0800

    Update register.php

--- register.php
+++ register.php
@@ -1,29 +1,27 @@
 <?php
+    include("config.php");
 
-include("config.php");
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// Create connection
-$conn = mysqli_connect($host, $username, $password, $dbname);
+    //POST = send/save data to database
+    //GET = get data from database
 
-//POST = send/save data to database
-//GET = get data from database
-
-$admin_email = $_POST["admin_email"];
-$admin_password = $_POST["admin_password"];
-$admin_name = $_POST["admin_name"];
-$admin_phone= $_POST["admin_phone"];
+    $admin_email = $_POST["admin_email"];
+    $admin_password = $_POST["admin_password"];
+    $admin_name = $_POST["admin_name"];
+    $admin_phone= $_POST["admin_phone"];
 
 
-if(empty($admin_email) || empty($admin_password) || empty($admin_name) || empty($admin_phone) ){
-    echo "Please fill in all the fields.";
-}
+    if(empty($admin_email) || empty($admin_password) || empty($admin_name) || empty($admin_phone) ){
+        echo "Please fill in all the fields.";
+    }
 
-$sql = "INSERT INTO admin (admin_email, admin_password, admin_name, admin_phone) VALUES ('$admin_email', '$admin_password', '$admin_name', '$admin_phone')";
+    $sql = "INSERT INTO admin (admin_email, admin_password, admin_name, admin_phone) VALUES ('$admin_email', '$admin_password', '$admin_name', '$admin_phone')";
 
     if (mysqli_query($conn, $sql)) {
         echo json_encode(["status" => "Success"]);
     } else {
         echo json_encode(["status" => "Error"]);
     }
-
 ?>
