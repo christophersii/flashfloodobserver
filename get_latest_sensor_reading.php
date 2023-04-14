45d72f0eba335bd67a0091184aab323fac3fb2c1
christophersii <116040417+christophersii@users.noreply.github.com>
2023-04-15 07:05:36 +0800

    Update get_latest_sensor_reading.php

--- get_latest_sensor_reading.php
+++ get_latest_sensor_reading.php
@@ -1,37 +1,32 @@
 <?php
-    header("Content-Type: application/json; charset=UTF-8");
-
-    // Retrieve POST data
-    $admin_id = $_POST['admin_id'];
-
     include 'config.php';
 
+    // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
     if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+      die("Connection failed: " . $conn->connect_error);
     }
 
-    // Query to get the latest water level reading for each station
-    $sql = "SELECT s.station_code, s.station_name, sr.water_level
-            FROM station s
-            LEFT JOIN sensor_device sd ON s.station_code = sd.station_code
-            LEFT JOIN sensor_reading sr ON sd.device_id = sr.device_id
-            WHERE s.admin_id = '$admin_id'
-            GROUP BY s.station_code
-            ORDER BY s.station_name";
+    $stationCode = $_POST["station_code"];
 
+    // Get latest water level reading for the specified station
+    $sql = "SELECT water_level FROM sensor_device sd
+            INNER JOIN sensor_reading sr ON sr.device_id = sd.device_id
+            WHERE sd.station_code = '$stationCode'
+            ORDER BY sr.reading_time DESC
+            LIMIT 1";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
-        // Convert MySQL result set to JSON format
-        $rows = array();
-        while ($row = $result->fetch_assoc()) {
-            $rows[] = $row;
-        }
-        echo json_encode($rows);
+      // Output latest water level reading as JSON
+      $row = $result->fetch_assoc();
+      echo json_encode(array("water_level" => $row["water_level"]));
     } else {
-        echo "0 results";
+      echo json_encode(array("error" => "No readings found for specified station"));
     }
 
     $conn->close();
+
 ?>
