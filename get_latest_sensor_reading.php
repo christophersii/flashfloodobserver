4d9f48dac88721dbfc280c526bd7a0bfb53b194c
christophersii <116040417+christophersii@users.noreply.github.com>
2023-04-15 06:57:03 +0800

    Update get_latest_sensor_reading.php

--- get_latest_sensor_reading.php
+++ get_latest_sensor_reading.php
@@ -1,45 +1,37 @@
 <?php
-require_once 'config.php';
+    header("Content-Type: application/json; charset=UTF-8");
 
-// Get all stations
-$sql = "SELECT * FROM station WHERE admin_id = ?";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("s", $_POST['admin_id']);
-$stmt->execute();
-$result = $stmt->get_result();
+    // Retrieve POST data
+    $admin_id = $_POST['admin_id'];
 
-$stations = array();
-while ($row = $result->fetch_assoc()) {
-    $station = array(
-        "station_code" => $row['station_code'],
-        "station_name" => $row['station_name']
-    );
-    
-    // Get latest sensor reading for station
-    $sql2 = "SELECT water_level, reading_time FROM sensor_reading 
-             INNER JOIN sensor_device ON sensor_reading.device_id = sensor_device.device_id
-             WHERE sensor_device.station_code = ?
-             ORDER BY reading_time DESC
-             LIMIT 1";
-    $stmt2 = $conn->prepare($sql2);
-    $stmt2->bind_param("s", $row['station_code']);
-    $stmt2->execute();
-    $result2 = $stmt2->get_result();
-    
-    if ($result2->num_rows > 0) {
-        $row2 = $result2->fetch_assoc();
-        $station["water_level"] = $row2["water_level"];
-        $station["reading_time"] = $row2["reading_time"];
-    } else {
-        $station["water_level"] = "N/A";
-        $station["reading_time"] = "N/A";
+    include 'config.php';
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
     }
-    
-    $stations[] = $station;
-}
 
-echo json_encode($stations);
-$stmt->close();
-$stmt2->close();
-$conn->close();
+    // Query to get the latest water level reading for each station
+    $sql = "SELECT s.station_code, s.station_name, sr.water_level
+            FROM station s
+            LEFT JOIN sensor_device sd ON s.station_code = sd.station_code
+            LEFT JOIN sensor_reading sr ON sd.device_id = sr.device_id
+            WHERE s.admin_id = '$admin_id'
+            GROUP BY s.station_code
+            ORDER BY s.station_name";
+
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // Convert MySQL result set to JSON format
+        $rows = array();
+        while ($row = $result->fetch_assoc()) {
+            $rows[] = $row;
+        }
+        echo json_encode($rows);
+    } else {
+        echo "0 results";
+    }
+
+    $conn->close();
 ?>
