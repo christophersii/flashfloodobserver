462d7cfd68beeaf3e5122842d13539d708c39a36
christophersii <116040417+christophersii@users.noreply.github.com>
2023-04-21 05:30:27 +0800

    Create database.php

---
+++ database.php
@@ -0,0 +1,24 @@
+<?php
+class Database {
+    // Specify your own database credentials here
+    private $host = "us-cdbr-east-06.cleardb.net"
+    private $db_name = "heroku_3442ee38bf9fb24";
+    private $username = "b005c8a97ae61d";
+    private $password = "04eca8ce";
+    public $conn;
+
+    // Get the database connection
+    public function getConnection() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        } catch(PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
+?>
