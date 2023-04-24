32523808adfc4cbbeb02c3aa4429e405e5ab960e
christophersii <116040417+christophersii@users.noreply.github.com>
2023-04-25 07:24:39 +0800

    Update get_stations.php

--- get_stations.php
+++ get_stations.php
@@ -1,34 +1,40 @@
 <?php
-
-  // Include the database connection details
-  include('config.php');
-
- // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+// Database connection details
+include 'config.php';
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 }
 
 // Get admin_id from POST request
 $admin_id = $_POST['admin_id'];
 
-// SQL query to fetch stations for the logged-in admin user
-$sql = "SELECT * FROM station WHERE admin_id = '$admin_id'";
+// Prepare SQL query to fetch the stations
+$sql = "SELECT station_code, station_name FROM stations WHERE admin_id = ? ORDER BY station_name";
 
-$result = $conn->query($sql);
+// Prepare and execute the SQL statement
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $admin_id);
+$stmt->execute();
 
-if ($result->num_rows > 0) {
-    // Output data of each row
-    $rows = array();
-    while($row = $result->fetch_assoc()) {
-        $rows[] = $row;
-    }
-    echo json_encode($rows);
-} else {
-    echo "0 results";
+// Bind the result to variables
+$stmt->bind_result($station_code, $station_name);
+
+// Fetch the results and store them in an array
+$results = array();
+while ($stmt->fetch()) {
+  $results[] = array(
+    'station_code' => $station_code,
+    'station_name' => $station_name
+  );
 }
 
+// Close the statement and connection
+$stmt->close();
 $conn->close();
+
+// Return the results as a JSON object
+header('Content-Type: application/json');
+echo json_encode($results);
 ?>
