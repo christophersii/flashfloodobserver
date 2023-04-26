77b7803a33cf626eb98c0d70af4fb01b996b5a27
christophersii <116040417+christophersii@users.noreply.github.com>
2023-04-27 05:39:40 +0800

    Update get_notifications.php

--- get_notifications.php
+++ get_notifications.php
@@ -6,7 +6,11 @@ $response = array();
 if (isset($_POST['admin_id'])) {
     $admin_id = $_POST['admin_id'];
 
-    $sql = "SELECT * FROM adminnotification WHERE admin_id = ? ORDER BY noti_time DESC";
+    $sql = "SELECT an.admin_notify_id, an.notify_info, an.noti_time, an.device_id 
+            FROM adminnotification an
+            INNER JOIN sensor_device sd ON an.device_id = sd.device_id
+            WHERE sd.admin_id = ?
+            ORDER BY an.noti_time DESC";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("i", $admin_id);
     $stmt->execute();
