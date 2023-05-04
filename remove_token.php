3eb613b6694c2c80046412fae4cca5cd4182348a
christophersii <116040417+christophersii@users.noreply.github.com>
2023-05-04 16:26:29 +0800

    Create remove_token.php

---
+++ remove_token.php
@@ -0,0 +1,26 @@
+<?php
+require_once('config.php');
+
+function removeTokenFromDB($token, $conn) {
+    $removeToken = "DELETE FROM fcm_tokens WHERE token='$token'";
+    if (mysqli_query($conn, $removeToken)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $tokensToRemove = $_POST['tokens'];
+
+    foreach ($tokensToRemove as $token) {
+        if (removeTokenFromDB($token, $conn)) {
+            echo "Removed token: $token\n";
+        } else {
+            echo "Error removing token: $token\n";
+        }
+    }
+    
+    mysqli_close($conn);
+}
+?>
