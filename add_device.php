540974105cef4b542629fc6d65b05cd8d4e4d4c4
christophersii <116040417+christophersii@users.noreply.github.com>
2023-05-06 08:37:24 +0800

    Update add_device.php

--- add_device.php
+++ add_device.php
@@ -4,9 +4,9 @@ require_once 'config.php';
 
 $response = array('success' => false);
 
-if (isset($_POST['device_id']) && isset($_POST['admin_id']) && isset($_POST['station_name']) && isset($_POST['latitude']) && isset($_POST['longitude']) && isset($_POST['drainage_depth']) && isset($_POST['threshold_alert']) && isset($_POST['threshold_warning']) && isset($_POST['threshold_danger'])) {
+if (isset($_POST['device_id']) && isset($_POST['admin_sensor_device_id']) && isset($_POST['station_name']) && isset($_POST['latitude']) && isset($_POST['longitude']) && isset($_POST['drainage_depth']) && isset($_POST['threshold_alert']) && isset($_POST['threshold_warning']) && isset($_POST['threshold_danger'])) {
     $device_id = $_POST['device_id'];
-    $admin_id = $_POST['admin_id'];
+    $admin_id = $_POST['admin_sensor_device_id'];
     $station_name = $_POST['station_name'];
     $latitude = $_POST['latitude'];
     $longitude = $_POST['longitude'];
@@ -25,11 +25,21 @@ if (isset($_POST['device_id']) && isset($_POST['admin_id']) && isset($_POST['sta
     if ($stmt->execute()) {
         $station_code = $conn->insert_id;
 
-        $stmt2 = $conn->prepare("INSERT INTO sensor_device (device_id, admin_id, station_code) VALUES (?, ?, ?)");
-        $stmt2->bind_param("sii", $device_id, $admin_id, $station_code);
+        $stmt2 = $conn->prepare("INSERT INTO sensor_device (device_id, station_code) VALUES (?, ?)");
+        $stmt2->bind_param("si", $device_id, $station_code);
 
         if ($stmt2->execute()) {
-            $response['success'] = true;
+            $device_id_fk = $conn->insert_id;
+
+            $stmt3 = $conn->prepare("INSERT INTO admin_sensor_device (device_id, admin_id) VALUES (?, ?)");
+            $stmt3->bind_param("ii", $device_id_fk, $admin_id);
+
+            if ($stmt3->execute()) {
+                $response['success'] = true;
+            } else {
+                $response['message'] = "Error inserting admin_sensor_device: " . $stmt3->error;
+            }
+            $stmt3->close();
         } else {
             $response['message'] = "Error inserting device: " . $stmt2->error;
         }
@@ -43,3 +53,4 @@ if (isset($_POST['device_id']) && isset($_POST['admin_id']) && isset($_POST['sta
 
 echo json_encode($response);
 
+?>
