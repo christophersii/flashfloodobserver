7e7bf02a200057823646c9f0fb838b73ddfc2ecb
christophersii <116040417+christophersii@users.noreply.github.com>
2023-05-06 09:20:10 +0800

    Update add_device.php

--- add_device.php
+++ add_device.php
@@ -4,16 +4,9 @@ require_once 'config.php';
 
 $response = array('success' => false);
 
-if (isset($_POST['device_id']) && isset($_POST['admin_ids']) && isset($_POST['station_name']) && isset($_POST['latitude']) && isset($_POST['longitude']) && isset($_POST['drainage_depth']) && isset($_POST['threshold_alert']) && isset($_POST['threshold_warning']) && isset($_POST['threshold_danger'])) {
+if (isset($_POST['device_id']) && isset($_POST['admin_id']) && isset($_POST['station_name']) && isset($_POST['latitude']) && isset($_POST['longitude']) && isset($_POST['drainage_depth']) && isset($_POST['threshold_alert']) && isset($_POST['threshold_warning']) && isset($_POST['threshold_danger'])) {
     $device_id = $_POST['device_id'];
-    $admin_ids_json = $_POST['admin_ids'];
-    $admin_ids = json_decode($admin_ids_json, true);
-    // Check if the decoded value is an array
-    if (!is_array($admin_ids)) {
-        http_response_code(400);
-        echo json_encode(['error' => 'Invalid admin_ids value.']);
-        exit();
-    }
+    $admin_id = $_POST['admin_id'];
     $station_name = $_POST['station_name'];
     $latitude = $_POST['latitude'];
     $longitude = $_POST['longitude'];
@@ -26,61 +19,26 @@ if (isset($_POST['device_id']) && isset($_POST['admin_ids']) && isset($_POST['st
         die("Connection failed: " . $conn->connect_error);
     }
 
-    // Check if device_id already exists
-    $stmt_check = $conn->prepare("SELECT device_id FROM sensor_device WHERE device_id = ?");
-    $stmt_check->bind_param("s", $device_id);
-    $stmt_check->execute();
-    $stmt_check->store_result();
+    $stmt = $conn->prepare("INSERT INTO station (station_name, latitude, longitude, drainage_depth, threshold_alert, threshold_warning, threshold_danger, admin_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("sddssssi", $station_name, $latitude, $longitude, $drainage_depth, $threshold_alert, $threshold_warning, $threshold_danger, $admin_id);
 
-    if ($stmt_check->num_rows > 0) {
-        $response['message'] = "Error: device_id already exists";
-        $stmt_check->close();
-    } else {
-        $stmt_check->close();
+    if ($stmt->execute()) {
+        $station_code = $conn->insert_id;
 
-        // Proceed with the insertion
-        $stmt = $conn->prepare("INSERT INTO station (station_name, latitude, longitude, drainage_depth, threshold_alert, threshold_warning, threshold_danger) VALUES (?, ?, ?, ?, ?, ?, ?)");
-        $stmt->bind_param("sddssss", $station_name, $latitude, $longitude, $drainage_depth, $threshold_alert, $threshold_warning, $threshold_danger);
+        $stmt2 = $conn->prepare("INSERT INTO sensor_device (device_id, admin_id, station_code) VALUES (?, ?, ?)");
+        $stmt2->bind_param("sii", $device_id, $admin_id, $station_code);
 
-        if ($stmt->execute()) {
-            $station_code = $conn->insert_id;
-
-            $stmt2 = $conn->prepare("INSERT INTO sensor_device (device_id, station_code) VALUES (?, ?)");
-            $stmt2->bind_param("si", $device_id, $station_code);
-
-            if ($stmt2->execute()) {
-                $device_id_fk = $conn->insert_id;
-
-                $stmt3 = $conn->prepare("INSERT INTO admin_sensor_device (device_id, admin_id) VALUES (?, ?)");
-                $success_count = 0;
-
-                foreach ($admin_ids as $admin_id) {
-                    $stmt3->bind_param("ii", $device_id_fk, $admin_id);
-
-                    if ($stmt3->execute()) {
-                        $success_count++;
-                    }
-                }
-
-                if ($success_count == count($admin_ids)) {
-                    $response['success'] = true;
-                } else {
-                    $response['message'] = "Error inserting some admin_sensor_device entries: " . $stmt3->error;
-                }
-                
-                $stmt3->close();
-                } else {
-                    $response['message'] = "Error inserting device: " . $stmt2->error;
-                }
-            
-                $stmt2->close();
-                } else {
-                    $response['message'] = "Error inserting station: " . $stmt->error;
-                }
-            $stmt->close();
+        if ($stmt2->execute()) {
+            $response['success'] = true;
+        } else {
+            $response['message'] = "Error inserting device: " . $stmt2->error;
         }
+        $stmt2->close();
+    } else {
+        $response['message'] = "Error inserting station: " . $stmt->error;
+    }
+    $stmt->close();
     $conn->close();
 }
 
 echo json_encode($response);
-?>
