36e5df4bc34b1bacfab1cf2608ca671f29137bf8
christophersii <116040417+christophersii@users.noreply.github.com>
2023-05-06 11:03:47 +0800

    Update save_token.php

--- save_token.php
+++ save_token.php
@@ -3,14 +3,15 @@ require_once('config.php');
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $token = $_POST['token'];
-    
+    $admin_id = $_POST['admin_id'];
+
     // Check if token already exists
     $checkToken = "SELECT * FROM fcm_tokens WHERE token='$token'";
     $result = mysqli_query($conn, $checkToken);
-    
+
     if (mysqli_num_rows($result) == 0) {
         // Token doesn't exist, insert it into the database
-        $insertToken = "INSERT INTO fcm_tokens (token) VALUES ('$token')";
+        $insertToken = "INSERT INTO fcm_tokens (token, admin_id) VALUES ('$token', '$admin_id')";
         if (mysqli_query($conn, $insertToken)) {
             echo json_encode(['message' => 'Token saved']);
         } else {
@@ -19,7 +20,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     } else {
         echo json_encode(['message' => 'Token already exists']);
     }
-    
+
     mysqli_close($conn);
 }
 ?>
