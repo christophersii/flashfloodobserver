f31759960a485e56816681edf1bbaa08b94b4465
Christopher Sii How Chiong <69385@siswa.unimas.my>
2023-05-28 22:14:02 +0800

    Update fetch_station.php

--- fetch_station.php
+++ fetch_station.php
@@ -12,9 +12,15 @@ if ($conn->connect_error) {
 $device_id = $_GET['device_id'];
 $admin_id = $_GET['admin_id'];
 
-// Fetch the station data using the device_id and admin_id
-$sql = "SELECT st.* FROM sensor_device sd
+// Fetch the station data and latest connectivity_type using the device_id and admin_id
+$sql = "SELECT st.*, sr.connectivity_type FROM sensor_device sd
         JOIN station st ON sd.station_code = st.station_code
+        LEFT JOIN (
+          SELECT device_id, MAX(reading_time) as MaxTime
+          FROM sensor_reading
+          GROUP BY device_id
+        ) sr1 on sr1.device_id = sd.device_id
+        LEFT JOIN sensor_reading sr on sr.device_id = sr1.device_id and sr.reading_time = sr1.MaxTime
         WHERE sd.device_id = '$device_id' AND sd.admin_id = '$admin_id'";
 $result = $conn->query($sql);
 
