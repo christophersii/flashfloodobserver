04203e9b1553c86746675a7cd85c5163166c2be0
Christopher Sii How Chiong <69385@siswa.unimas.my>
2023-06-21 16:48:33 +0800

    Update connection.php

--- wanyu/connection.php
+++ wanyu/connection.php
@@ -1,8 +1,13 @@
 <?php
-$host = "localhost";
-$username = "id20548474_fyp_flashflood";
-$password = "053591382aA@";
-$dbname = "id20548474_fyp_project";
+// $host = "localhost";
+// $username = "id20548474_fyp_flashflood";
+// $password = "053591382aA@";
+// $dbname = "id20548474_fyp_project";
+
+$host = "us-cdbr-east-06.cleardb.net";
+$username = "b005c8a97ae61d";
+$password = "04eca8ce";
+$dbname = "heroku_3442ee38bf9fb24";
 
 // Create connection
 $conn = mysqli_connect($host, $username, $password, $dbname);
